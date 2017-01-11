962bf90250f39207d532494c055e32cfec3011ad
mostalt <mostalt.mail@gmail.com>
2017-01-11 21:27:33 +0300

    add loader

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,4 +1,4 @@
-import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES } from '../constants'
+import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES, START, FAIL, SUCCESS } from '../constants'
 import { normalizedArticles } from '../fixtures'
 import { arrayToMap } from '../store/helpers'
 import { Record, Map } from 'immutable'
@@ -11,17 +11,29 @@ const ArticleModel = Record({
   comments: []
 })
 
-export default (articles = new Map({}), action) => {
+const defaultState = new Map({
+  entities: new Map({}),
+  loading: false,
+  loaded: false
+})
+
+export default (articles = defaultState, action) => {
   const { type, payload, generatedId, response } = action
   switch (type) {
     case DELETE_ARTICLE:
-      return articles.delete(payload.id)
+      return articles.deleteIn(['entities', payload.id])
     
     case ADD_COMMENT:
-      return articles.updateIn([payload.articleId, 'comments'], comments => comments.concat(generatedId))
+      return articles.updateIn(['entities', payload.articleId, 'comments'], comments => comments.concat(generatedId))
     
-    case LOAD_ALL_ARTICLES:
-      return articles.merge(arrayToMap(response, article => new ArticleModel(article)))
+    case LOAD_ALL_ARTICLES + START:
+      return articles.set('loading', true)
+    
+    case LOAD_ALL_ARTICLES + SUCCESS:
+      return articles
+        .set('entities', arrayToMap(response, article => new ArticleModel(article)))
+        .set('loading', false)
+        .set('loaded', true)
 
   }
   return articles
