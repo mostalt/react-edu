0b1192117a82c19fdf476f1678457b9453aa1c90
mostalt <mostalt.mail@gmail.com>
2017-01-24 20:25:15 +0300

    add index route and redirects

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, Redirect, IndexRedirect, IndexRoute, hashHistory, browserHistory } from 'react-router'
 import Container from './components/Container'
 import NewArticleForm from './components/NewArticleForm.js'
 import Filter from './components/Filter'
@@ -9,15 +9,20 @@ import ArticlePage from './routeHandlers/ArticlePage'
 import NotFoundPage from './routeHandlers/NotFoundPage'
 import CommentsRoot from './routeHandlers/CommentsRoot'
 import CommentsPage from './routeHandlers/CommentsPage'
+import ArticleIndexPage from './routeHandlers/ArticleIndexPage'
 
 export default <Router history={browserHistory}>
   <Route path = "/" component = {Container}>
+    <IndexRedirect to = "/articles" />
+    <Redirect from = "article" to = "articles" />
     <Route path = "articles" component = {ArticleList}>
+      <IndexRoute component = {ArticleIndexPage} />
       <Route path = "new" component = {NewArticleForm} />
       <Route path = ":id" component = {ArticlePage}/>
     </Route>
     <Route path = "/filter" component={Filter} />
     <Route path = "comments" component = {CommentsRoot}>
+      <IndexRedirect to = "1" />
       <Route path = ":page" component = {CommentsPage} />
     </Route>
     <Route path = "*" component = {NotFoundPage} />
